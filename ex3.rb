debeee6421f80ada52cd16651a860ace4e06f5bf
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-08 00:03:45 -0500

    first three exercises

---
+++ ex3.rb
@@ -0,0 +1,26 @@
+puts "I will now count my chickens:" # prints on a new line
+
+puts "Hens #{25 + 30 / 6}" # 25 + 30 / 6 -> 25 + 5 -> 30
+puts "Roosters #{100 - 25 * 3 % 4}" # 100 - 25 * 3 % 4 -> 100 - 3 -> 97
+
+puts "How I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2  - 1 / 4.0 + 6
+# 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# 3 + 2 + 1 - 5 + 0 - .25 + 6
+# 6.75
+
+puts "Is 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 # 5 < -2 -> false
+
+puts "3 + 2 is #{3 + 2}"
+puts "5 - 7 is #{5 - 7}"
+
+puts "That's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
