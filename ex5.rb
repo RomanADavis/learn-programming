fcaa865a2c9d9a9af80ce59e21b1a9ee3057b8f3
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-08 22:03:07 -0500

    exercise five

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = "Roman Davis"
+my_age = 27 # in 2016
+my_height = 5 * 12 + 5 # in inches
+my_weight = 160 # pounds
+my_eyes = "brown"
+my_teeth = "white"
+my_hair = "brown"
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He weighs #{my_weight} pounds."
+puts "I need to get back in shape."
+puts "His teeth are #{my_teeth}, hopefully."
+
+# Try not to let your attention lapse.
+puts "If I add #{my_age},  #{my_height} and #{my_weight}, I get #{my_age + my_height + my_weight}."
