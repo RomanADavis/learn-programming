c12f5280dfdf84c824769a1d93c24a828054be3b
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-12 03:45:25 -0500

    Exercise 23

---
+++ ex23.rb
@@ -0,0 +1,6 @@
+# Started looking at some ruby code of an emulator at
+# https://github.com/colby-swandale/waterfoul/blob/master/lib/waterfoul/instructions/jump.rb
+# I don't understand it exactly. I mean, I get that these are methods that are
+# supposed to be called if the emulator as op codes when the program is fed in a
+# certain number from the rom, but there's a lot to this program. I'll have to
+# come back to it later.
