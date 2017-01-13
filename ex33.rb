226f19c4758622880742690da5a0fafa8ac4d91a
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-13 11:11:10 -0500

    Exercise 33

--- ex33.rb
+++ ex33.rb
@@ -1,15 +1,20 @@
-i = 0
-numbers = []
 
-while i < 6
-  puts "At the top i is #{i}."
-  numbers << i
 
-  i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+
+def count_to(n, by = 1) # will run forever if by is set to 0 or less.
+  numbers = []
+  i = 0
+  while i < n
+    puts "At the top i is #{i}."
+    numbers << i
+
+    i += by
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+  end
+  numbers
 end
 
 puts "The numbers:"
 
-numbers.each {|number| puts number}
+count_to(6).each {|number| puts number}
