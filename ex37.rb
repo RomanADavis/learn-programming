85993b4fdc2023729bb2b70759071efa9dbb9326
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-13 14:56:14 -0500

    Exercise 37 & TODO list

---
+++ ex37.rb
@@ -0,0 +1,84 @@
+#### TODO: Make a simple flash card style game
+#### TODO: Get serious reading code
+## Keywords ##
+# BEGIN{} Runs a block when the program starts
+# END{} Runs a block when the program ends
+# alias Creates another name for a method
+# and is a logical && with lower priority and discouraged
+# begin starts a block, used for exceptions
+# break breaks out of a loop
+# case is a style of conditional
+# class defines a class
+# def defines a methods
+# defined? returns true if a method or class is defined; false otherwise
+# do creates a block that may take a parameter
+# else conditional
+# elsif conditional
+# end ends blocks, classes, methods, etc.
+# ensure runs code even if there is an exeception
+# for discouraged for-loop syntax
+# if conditional
+# in is (only?) used in for loops
+# module defines a module
+# next breaks out of current iteration of a loops
+# not discouraged version of !
+# or discouraged version of ||
+# redo rerun a codeblock exactly the same
+# rescue{} runs a block if an exception happens
+# retry tries to run a block again if an excpetion occurs
+# return returns a value from a method; optional on the last line
+# self returns the current classm object or module
+# super returns the paren of the current classm
+# then is optionally used witn if
+# undef removes a method from a classm
+# unless runs a block if it's argument reduces to false or nil
+# until runs a block until it's argument isn't false or nil
+# when is used like if inside of a case statement
+# while executes a block until it's argument is false or nil
+# yield transfers control to a method's block
+## Data Types ##
+# boolean can be true or false
+# nil represents an undefined valu
+# strings store character between ""
+# Integer store whole numbers
+# Float store decimal numbers
+# Array store a list of other data types
+# Hash stores key-value pairs
+## Escape Sequences ##
+# \\ backslash
+# \' single quote
+# \" double quote
+# \a bell
+# \b backspace
+# \f formfeed (little square)
+# \n newline
+# \r carrige
+# \t tab
+#\v vertical tab
+## Operators (Really just fancy methods in ruby) ##
+# + add
+# - subtract
+# * multiply
+# ** power of
+# / divide
+# % modulo
+# > greater than
+# . dot access
+# :: colon access
+# [] list brackets
+# ! not
+# < less than
+# <= less than or equal to
+# >= greater than or equal to
+# > greater than
+# <=> comparison Operator
+# == equal
+# === equality
+# != does not equal
+# && logical and
+# || logical or
+# .. inclusive range
+# ... exclusive range
+# @ object scope
+# @@ class scope
+# $ global scope
