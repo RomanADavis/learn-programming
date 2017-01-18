cf40baec51ae4aba507a27dd7bf750ab70e20643
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-17 23:47:48 -0500

    incremental change

---
+++ learn_ruby_the_hard_way/gothomweb/lib/gothomweb/room.rb
@@ -0,0 +1,16 @@
+class Room
+  attr_reader :name, :description, :paths
+  def initialize(name, description)
+    @name = name
+    @description = description
+    @paths = {}
+  end
+
+  def go(direction)
+    @paths[direction]
+  end
+
+  def add_paths(paths)
+    @paths.update(paths)
+  end
+end
