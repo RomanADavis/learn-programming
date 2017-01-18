4f4ef05990ea05f8bb84646dfe1f7d042e94c403
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-17 23:57:40 -0500

    Room class for gothomweb. Added some rooms in Map module.

---
+++ learn_ruby_the_hard_way/gothomweb/lib/gothomweb/map.rb
@@ -0,0 +1,22 @@
+module Map
+  CENTRAL_CORRIDOR = Room.new("Central Corridor",
+  """
+  The Gothons of Planet Percal #25 have invaded your ship and destroyed your
+  entire crew. You are the last surviving member and your last mission is to
+  get the nuetron destruct bomb from the Weapons Armory, put it in the bridge,
+  and blow the ship up after getting into an escape pod.
+
+  You're running down the Central Corridor to the Weapons Armory when a Gothon
+  jumps out, red scaly skin, dark grimy teeth, and evil clown costume flowing
+  around his hate filled body. He's blocking the door to the Armory and about to
+  pull out a weapon to blast you.
+  """)
+
+  LASER_WEAPON_ARMORY = Room.new("Laser Weapon Armory",
+  """
+  """)
+
+  THE_BRIDGE = Room.new("The Bridge",
+  """
+  """)
+end
