0ff0316ae8261e3f77e238a6ab76959cd3229978
Roman Davis <FluffyRibbit@gmail.com>
2017-02-07 15:28:36 +0000

    Added error messages

--- hartl-rails/sample-app/app/controllers/users_controller.rb
+++ hartl-rails/sample-app/app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if User.save
+    if @user.save
       #Handle a successful save
     else
       render 'new'
