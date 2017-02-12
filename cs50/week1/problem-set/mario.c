6d167a596a06b8a14ad94e2087e35ffa70c69e57
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-12 22:02:19 +0000

    Rewrote mario for the more comfortable version. Compiled, checked and added comments

--- cs50/week1/problem-set/mario.c
+++ cs50/week1/problem-set/mario.c
@@ -5,13 +5,17 @@
 int get_height();
 void draw_pyramid(int);
 void draw_layer(int, int);
+void draw_stack(int);
+void alignment(int, int);
 
+// Takes in a height and prints out a double pyramid
 int main(void){
     int height = get_height();
     
     draw_pyramid(height);
 }
 
+// Gets the height from the user, reject negative numbers and numbers over 23.
 int get_height(){
     int height;
     
@@ -23,20 +27,33 @@ int get_height(){
     return height;
 }
 
+// Draws the double pyramid in a loop.
 void draw_pyramid(int height){
     for(int layer = 0; layer < height; layer++){
         draw_layer(layer, height);
     }
 }
 
+// Draws the layers by calling alignment() and draw_stack()
 void draw_layer(int layer, int height){
+    alignment(layer, height);
+    draw_stack(layer);
+    printf("  ");
+    draw_stack(layer);
+    printf("\n");
+}
+
+
+// Aligns the pyramid by drawing spaces one character at a time for that layer.
+void alignment(int layer, int height){
     for(int i = 0; i < height - layer - 1; i++){
         printf(" ");
     }
-    
+}
+
+// Draws the stack by printing "#" over and over for that layer.
+void draw_stack(int layer){
     for(int i = 0; i <= layer; i++){
         printf("#");
-    }
-    
-    printf("#\n");
+    }    
 }
