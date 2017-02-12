503a6f2c3811b19ea57c71bb5bdde1bbbc4c8ae9
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-12 21:04:17 +0000

    Added comments to problem set.

--- cs50/week1/problem-set/water.c
+++ cs50/week1/problem-set/water.c
@@ -1,9 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+// Calculates the equivalent number in bottles of water from minutes in the shower.
 
 long bottles(long);
 long double_to_long(double);
 
+// Takes in input and displays result.
 int main(void){
     printf("Minutes: ");
     int minutes = get_int();
@@ -11,6 +13,7 @@ int main(void){
     printf("Bottles: %ld\n", bottles(minutes));
 }
 
+// Calculates the number of gallons per minute.
 long bottles(long minutes){
     int ounces_in_a_gallon = 128, ounces_in_a_bottle = 16;
     float gallons_per_minute = 1.5;
@@ -20,6 +23,7 @@ long bottles(long minutes){
     return double_to_long(gallons * ounces_in_a_gallon / ounces_in_a_bottle);
 }
 
+// Converts doubles (decimal numbers) to longs (integes)
 long double_to_long(double f){
     return f >= 0 ? (long) (f + 0.5) : (long) (f - 0.5);
 }
