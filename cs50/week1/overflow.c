493479d47c15414e1f2a68c31f65d7811aa5fdb3
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-12 11:35:47 +0000

    Wrote and compiled impercision.c

--- cs50/week1/overflow.c
+++ cs50/week1/overflow.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 int main(void){
-    int n = 1;
-    for(int i = 0; i < 32; i++){
-        printf("%d\n", n);
+    long long  n = 1;
+    for(int i = 0; i < 64; i++){
+        printf("%lld\n", n);
         n *= 2;
     }
 }
