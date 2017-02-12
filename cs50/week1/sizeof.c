4dc5fa08fd892959cad4a7e7d72ee59fd5dd6cb2
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-12 10:37:29 +0000

    Wrote and compiled floats.c and sizeof.c

---
+++ cs50/week1/sizeof.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    printf("bool is size of %lu.\n", sizeof(bool));
+    printf("int is size of %lu.\n", sizeof(int));
+    printf("float is size of %lu.\n", sizeof(float));
+    printf("long is size of %lu.\n", sizeof(long));
+    printf("double is size of %lu.\n", sizeof(double));
+    printf("char is size of %lu.\n", sizeof(char));
+    printf("string is size of %lu.\n", sizeof(string));
+    
+}
