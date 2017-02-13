155499d958603894043ad90d8766948f56825783
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-13 05:01:23 +0000

    Cleaned up and added comments to credit.c

--- cs50/week1/problem-set/credit.c
+++ cs50/week1/problem-set/credit.c
@@ -2,13 +2,17 @@
 #include <cs50.h>
 #include <math.h>
 
-// I haven't been able to confirm if this algo works. It won't 
+// This code checks for valid and invalid Credit Card numbers. It was the most challenging C code 
+// I've ever done and took me all day.
+
 bool is_valid(long long);
 int digit_count(long long);
 int first_digit(long long);
 int isolate_digit(long long, int);
 void find_card(long long);
 
+// Prompts the user for a credit card, checks for validity. If valid, it checks for the card's
+// brand.
 int main(void){
     printf("Number:");
     long long card_number = get_long_long();
@@ -20,27 +24,26 @@ int main(void){
     }
 }
 
+// Checks if a card is valid.
 bool is_valid(long long number){
     int digits, total = 0;
     
     for(int i = 1; i <= digit_count(number); i += 2){
         digits = isolate_digit(number, i) * 2;
-        //printf("%d\n", isolate_digit(number, i));
        
         for(int j = 0;j <= digit_count(digits); j++){
             total += isolate_digit(digits, j);
-            //printf("%d\n", total);
         }
     }
     
     for(int i = 0; i <= digit_count(number); i += 2){
         total += isolate_digit(number, i);
     }
-    //printf("%d\n", total);
     
     return total % 10 == 0;
 }
 
+// Finds the length of an integer in decimal.
 int digit_count(long long number){
     long long max = 1;
     int digits = 0;
@@ -53,6 +56,7 @@ int digit_count(long long number){
     return digits;
 }
 
+// Returns a single digit of a decimal number.
 // place is defined from right to left.
 int isolate_digit(long long number, int place){
     long long top = 1;
@@ -64,16 +68,16 @@ int isolate_digit(long long number, int place){
     return number / top % 10;
 }
 
+// Returns the first digit.
 int first_digit(long long number){
     return isolate_digit(number, digit_count(number) - 1);
 }
 
+// Prints out the card type.
 void find_card(long long number){
     int first = first_digit(number);
     int length = digit_count(number);
     int second = isolate_digit(number, length - 2);
-    
-    //printf("%d %d %d", first, second, length);
     
     if(length == 15){
         if(first == 3 && (second == 4 || second == 7)){
