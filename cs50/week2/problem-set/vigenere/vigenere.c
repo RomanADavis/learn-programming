9f88466973c73ecb3b54a50d4ee7a32048ad07d2
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-14 00:20:41 +0000

    Mostly works. Need to make slight changes to pass tests

--- cs50/week2/problem-set/vigenere/vigenere.c
+++ cs50/week2/problem-set/vigenere/vigenere.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 bool is_alpha(string);
 char increment(char);
+void encrypt(string, string);
 
 int main(int argc, string argv[]){
     // Shuts down the program without a valid key.
@@ -12,13 +14,30 @@ int main(int argc, string argv[]){
         return 1;
     }
     
-    //string key = argv[1];
-    
+    string key = argv[1];
     string secret = get_string();
     
-    printf("%c\n", increment(secret[0]));
+    encrypt(secret, key);
 }
 
+void encrypt(string secret, string key){
+    int place = 0, key_length = strlen(key);
+    char letter;
+    
+    for(int i = 0, length = strlen(secret); i < length; i++){
+        letter = toupper(key[place]);
+        
+        if(('A' <= secret[i] && 'Z' >= secret[i]) || ('a' <= secret[i] && 'z' >= secret[i])){
+            for(char j = letter; 'A' < j; j--){
+                secret[i] = increment(secret[i]);
+            }
+            place = place < key_length - 1? place + 1 : 0;
+        }
+        
+        printf("%c", secret[i]);
+    }
+    printf("\n");
+}
 
 // Checks for key made of alphabetical characters
 bool is_alpha(string key){
@@ -31,6 +50,7 @@ bool is_alpha(string key){
     return true;
 }
 
+// Increments a letter, perserves case and non letter characters.
 char increment(char letter){
     if(letter >= 'a' && letter <= 'z'){
         return letter == 'z' ? 'a' : letter + 1;
