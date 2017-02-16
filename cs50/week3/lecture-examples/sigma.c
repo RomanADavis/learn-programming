8782ba13a7f477ee8fec4e8dd510f0840efe60f5
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-16 04:19:13 +0000

    guess at sigma

--- cs50/week3/lecture-examples/sigma.c
+++ cs50/week3/lecture-examples/sigma.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int sigma(int m);
+
 int main(void){
+    int n;
     
+    do{
+        printf("Positive integer please: ");
+        n = get_int(); 
+    }while(n < 0);
+    
+    int answer = sigma(n);
+    
+    printf("%d\n", answer);
+}
+
+int sigma(int m){ // Recursive algo for triangle numbers
+    if(m < 2){
+        return m;
+    }
+    
+    return m + sigma(m - 1);
 }
