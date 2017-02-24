d42a90aaf5b8cbb80a18a38a1122ffc6147d53d7
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-24 13:02:52 +0000

    changed a variable name. added comments. magically started working

--- cs50/week4/problem-set/recover/recover.c
+++ cs50/week4/problem-set/recover/recover.c
@@ -5,46 +5,56 @@
 
 typedef uint8_t BYTE;
 
-bool is_jpeg(BYTE*);
+bool is_jpg(BYTE*);
 
 int main(int argc, char *argv[]){
+    // Checks for argument
     if(argc != 2){
         fprintf(stderr, "Usage: ./recover raw.raw\n");
         return 1;
     }
     
+    // Checks that file cah be opened
     FILE *raw = fopen(argv[1], "r");
-    
     if(raw == NULL){
+        fclose(raw);
         fprintf(stderr, "Could not open %s\n", argv[1]);
         return 2;
     }
     
-    FILE *jpeg;
+    // Initialize variables
+    FILE *jpg;
     BYTE block[512];
-    char filename[7];
+    char filename[8];
     int filecount = -1, block_size;
     
-    do{
+    do{ // Skips bytes until a jpeg is found
        block_size = fread(&block, 1, 512, raw);
-        //printf("%x %x %x %x\n", block[0], block[1], block[3], block[4]);
-        //getch();
-    }while(!is_jpeg(block));
+    }while(!is_jpg(block));
     
-    while(block_size == 512){
-        filecount += 1;
+    while(block_size == 512){ // Runs until the whole the whole raw is read
+    
+        // Sets up the file.
+        filecount++; 
         sprintf(filename, "%03d.jpg", filecount);
-        jpeg = fopen(filename, "w");
+        jpg = fopen(filename, "w");
+        if(jpg == NULL){
+            fprintf(stderr, "Could not create jpeg.\n");
+        }
+    
+        // Reads the raw until it finds a new jpg.
         do{
-            fwrite(block, 1, 512, jpeg);
+            fwrite(block, 1, 512, jpg);
             block_size = fread(&block, 1, 512, raw);
-        }while(!is_jpeg(block) && block_size == 512);
-        fclose(jpeg);
+        }while(!is_jpg(block) && block_size == 512);
+    
+        // Closes the file.
+        fclose(jpg);
     }
     
     return 0;
 }
 
-bool is_jpeg(BYTE *block){
+bool is_jpg(BYTE *block){
     return block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] & 0xf0) == 0xe0;
 }
