b8e9ec335af483681eca63ed3021710b56a77a5d
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-02-24 13:56:17 +0000

    wrote search into node.c

--- cs50/week5/lecture-examples/node.c
+++ cs50/week5/lecture-examples/node.c
@@ -1,4 +1,27 @@
+#include <cs50.h>
+
 typedef struct node{
     int value;
     struct node *next;
+}
+node;
+
+// both delete and remove and remove are highlighted (reserved?) words.
+void pop(node *list, int value){
+    
+}
+
+void insert(node *list, int value){
+    
+}
+
+bool search(node *list, int value){
+    node *pointer = list;
+    while(pointer != NULL){
+        if(pointer -> value == value){
+            return true;
+        }
+        pointer = pointer -> next;
+    }
+    return false
 }
