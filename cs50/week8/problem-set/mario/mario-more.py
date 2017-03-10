49604df9d16e9f1de8c86899bc8d2ddb42aea7b9
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-10 09:06:29 +0000

    Wrote comments for mario-more and fixed a silly redundancy

--- cs50/week8/problem-set/mario/mario-more.py
+++ cs50/week8/problem-set/mario/mario-more.py
@@ -1,3 +1,9 @@
+# Solution for the "more comfortable" version of mario.c in python
+# Spec here: http://docs.cs50.net/problems/mario/more/mario.html
+# Instructions to convert to python here:
+# http://docs.cs50.net/2017/x/psets/6/pset6.html
+# Code by Roman Davis
+
 import cs50
 
 def main():
@@ -7,9 +13,8 @@ def main():
 def draw_pyramid(layers):
     for layer in range(layers):
         alignment = " " * (layers - layer - 1)
-        left_pyramid = "#" * (layer + 1)
-        right_pyramid = "#" * (layer + 1)
-        print(alignment + left_pyramid + " " + right_pyramid)
+        pyramid = "#" * (layer + 1)
+        print(alignment + pyramid + " " + pyramid)
         
 if __name__ == "__main__":
     main()
