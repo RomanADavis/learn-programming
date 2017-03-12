0f745382a1f703c918cfd47504f0f64cd4dc28d5
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-12 05:12:48 +0000

    expressions

---
+++ YDKJS/chapter1/expressions.js
@@ -0,0 +1,14 @@
+// Statements are made of one or more expressions.
+// For example:
+a = b * 2;
+// Contains:
+2; // a literal value expression, which represents itself.
+b; // a variable expression, which represents its current value.
+b * 2; // an arithmatic expression, which does math.
+a = b * 2; //an assignment expression,  assigns the value returned from b * 2 to a
+// The way I've done them here, they could each count as expression statements or
+// "statements that stand alone."
+
+// The most common kind of statement is a "call statement" that calls some function
+// and any necessary arguments. Here's an example:
+alert( a );
