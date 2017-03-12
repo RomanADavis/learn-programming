39336b880587bd74e643e32259209f9a11ae51d0
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-12 08:41:50 +0000

    try

---
+++ YDKJS/chapter1/try.js
@@ -0,0 +1,8 @@
+// code tried in js console
+a = 21;
+b = a * 2;
+console.log( b ); // prints 42 in the console
+alert( a ); // Pop up with 42 as a message.
+// Pop up with a field to enter info
+age = prompt( "Please tell me your age:" );
+console.log( age );
