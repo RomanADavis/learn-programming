5e3213ab8becaa6d947b20996449b38c40614123
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-12 05:03:41 +0000

    statements

---
+++ YDKJS/chapter1/statements.js
@@ -0,0 +1,10 @@
+// An example of a statement
+a = b * 2;
+// a and b are variables that represent some value.
+// 2 is an example of value and represents itself, i.e. a literal value.
+// ; end js statements. This isn't always necessary, since the compiler
+// will add it again for you, but many programmers will snear at you for
+// ignoring it.
+// Program traslates to multiply the value represented by b, multiply it
+// by 2, and return the total to a, which will represent that value for
+// now.
