adb8eeafcd7d82f748059f161d781c7e606f54ef
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-12 10:52:25 +0000

    comments.js

---
+++ YDKJS/chapter1/comments.js
@@ -0,0 +1,10 @@
+// Single line comments start with //
+
+/* 
+You can also put comments on more than one line by starting
+with /* and ending with */
+
+// I rarely use multiline comments this way, but very rarely it's
+// nice to have a comment inside a line like
+
+lightspeed /*in miles per second*/ = 180_000;
