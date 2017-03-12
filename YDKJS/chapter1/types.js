2103a2bf2a2ab0ff695405ef81d2e83949cdb7f6
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-12 10:47:29 +0000

    types.js

---
+++ YDKJS/chapter1/types.js
@@ -0,0 +1,15 @@
+// Strings can be single or double quoted
+"Peace";
+'Peace';
+
+42; // Numbers
+
+true; // Bools
+
+// Explicit conversion
+var a = "42";
+var b = Number( b ); // Converts from string.
+
+// Implicit conversion
+a == b; // Returns true
+a === b; // falsn
