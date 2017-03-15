ecfa568cdd1b479414da50819a281ea531b581bf
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-15 08:30:01 +0000

    variables

---
+++ YDKJS/chapter2/examples/variables.js
@@ -0,0 +1,7 @@
+// Variable names work mostly like any other language.
+// Letters, numbers, undercores and dollar sign are
+// allowed. They can't strart with a digit. And you
+// should avoid reserve words.
+
+var null = 1;
+// => Uncaught SyntaxError: Unexpected token null
