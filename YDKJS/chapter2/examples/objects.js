f374302da3b80a284979659e5a7261acf64c4a92
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-15 03:32:09 +0000

    objects

---
+++ YDKJS/chapter2/examples/objects.js
@@ -0,0 +1,23 @@
+// Objects used this way look an awful lot like ruby hashes/
+// python dictionaries
+var obj = {
+    a: "hello world",
+    b: 42,
+    c: true
+}
+//=>undefined
+// There's a way to call up object variables like a
+// normal object.
+obj.a;
+//=>"hello world"
+obj.b
+//=>42
+obj.c;
+//=>true
+// But you can also call them up like a hash!
+obj["a"];
+//=>"hello world"
+obj["b"];
+//=>42
+obj["c"];
+//=>true
