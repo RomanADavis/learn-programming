4bede511eec7824a8255c64aa5af70e0f92f1046
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-15 08:52:53 +0000

    global scope

---
+++ YDKJS/chapter2/examples/global-scope.js
@@ -0,0 +1,11 @@
+// Remember to declare your variables or you
+// will accidentally make a global!
+
+function foo() {
+    a = 1; // no declaration
+}
+
+foo();
+
+a; // I accidentally a global
+//1
