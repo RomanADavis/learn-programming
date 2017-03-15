bf1f2c41142b3d97738e4fb51aac27072736b961
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-15 23:35:49 +0000

    non javascrypt

---
+++ YDKJS/chapter2/examples/non-js.js
@@ -0,0 +1,18 @@
+// JS is a scripting language. It's not very fast,
+// and if you need to build something complex or 
+// math-intensive, you might be better off with
+// another language.
+
+// One of the reasons people use JS is as the glue
+// holding other languages together.
+
+// Here's the document object, which is JS way of
+// interacting with the DOM, which is mostly
+// written in HTML.
+
+var el = document.getElementById( "foo" );
+
+// But the I/O functions, like console.log and
+// and alert are also written in other languages,
+// although this is something you can usually
+// ignore as a "system" library.
