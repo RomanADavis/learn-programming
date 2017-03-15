67c552ce5019694e7e71a6b6390d3f65e1102597
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-15 06:28:34 +0000

    explicit conversion

---
+++ YDKJS/chapter2/examples/implicit-conversion.js
@@ -0,0 +1,15 @@
+// Type conversion can happen explicitly or
+// implicitly.
+
+var a = "42";
+
+// In this line JS thinks multiplying a string doesn't
+// make any sense, so it converts the string into a
+// a number and then does it.
+
+var b = a * 1;
+undefined
+a;
+"42"
+b;
+42
