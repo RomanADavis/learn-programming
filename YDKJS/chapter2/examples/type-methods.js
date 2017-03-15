8b0651031168262b3ef98f6de0e8c462e36b0479
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-15 06:19:45 +0000

    built in type methods

---
+++ YDKJS/chapter2/examples/type-methods.js
@@ -0,0 +1,12 @@
+// JS has some built in methods for other types, too!
+
+var a = "hello world";
+// => undefined
+var b = 3.14159;
+// => undefined
+a.length; // Okay, that's not so weird.
+// => 11
+a.toUpperCase();
+// => "HELLO WORLD"
+b.toFixed(4); // Numbers have methods!?
+"3.1416"
