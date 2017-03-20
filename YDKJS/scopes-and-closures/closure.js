456202ee21c80f410432996df91153b338afbbb8
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-20 15:34:36 +0000

    added IIFE as an example of something without an observed closure.

--- YDKJS/scopes-and-closures/closure.js
+++ YDKJS/scopes-and-closures/closure.js
@@ -11,3 +11,17 @@ function setupBot(name, selector) {
 }
 undefined
 setupBot( "Closure Bot 1", "#bot_1" );
+
+// I think I sort of get that closure is access to scope after a function has
+// been called. But, right now, that seems sort of trivial. 
+
+// Still, IIFE are even more trivial.
+
+var a = 2;
+undefined
+(function  IIFE(){
+    console.log( a );
+})();
+// 2
+
+// There is no opportunity to observe an IIFE closure.
