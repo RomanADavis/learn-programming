1f74c021cf5b36da819573853af1648a2d4a4aea
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-23 16:56:22 +0000

    implicit lost

---
+++ YDKJS/this-and-objects/chapter2/implicit-lost.js
@@ -0,0 +1,37 @@
+// Sometimes the call-site is lost. This is weird and confusing.
+function foo() {
+	console.log( this.a );
+}
+
+function doFoo(fn) {
+	// `fn` is just another reference to `foo`
+
+	fn(); // <-- call-site!
+}
+
+var obj = {
+	a: 2,
+	foo: foo
+};
+
+var a = "oops, global"; // `a` also property on global object
+
+doFoo( obj.foo ); // "oops, global"
+// ???
+
+function foo() {
+	console.log( this.a );
+}
+
+var obj = {
+	a: 2,
+	foo: foo
+};
+
+var bar = obj.foo; // function reference/alias!
+
+var a = "oops, global"; // `a` also property on global object
+
+bar(); // "oops, global"
+// because you aren't calling the function from obj1, but the global scope!
+// it actually makes sense! But it's an easy mistake to make accidently.
