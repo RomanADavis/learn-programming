edde1d3188d40e376f6173a25057ac67a634e49c
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-23 12:56:17 +0000

    more modern modules

--- YDKJS/scopes-and-closures/modern-modules.js
+++ YDKJS/scopes-and-closures/modern-modules.js
@@ -21,12 +21,33 @@ var MyModules = (function Manager() {
 })();
 
 // Example
-MyModules.define( "foo", [], function(){
+MyModules.define( "bar", [], function(){
     function hello(who) {
         return "Let me introduce: " + who;
     }
-    
+
     return {
         hello: hello
     };
 } );
+
+MyModules.define( "foo", ["bar"], function(bar){
+    var hungry = "hippo";
+
+    function awesome() {
+        console.log( bar.hello( hungry ).toUpperCase() );
+    }
+
+    return {
+        awesome: awesome
+    };
+} );
+
+var bar = MyModules.get( "bar" );
+var foo = MyModules.get( "foo" );
+
+console.log(
+    bar.hello( "hippo" )
+); // Let me introduce: hippo
+
+foo.awesome(); // LET ME INTRODUCE: HIPPO
