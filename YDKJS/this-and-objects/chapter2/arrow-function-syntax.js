088ab49a891a109d5f2db8c7be6d32c009870403
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-03-28 03:40:34 +0000

    use case for arrow function

--- YDKJS/this-and-objects/chapter2/arrow-function-syntax.js
+++ YDKJS/this-and-objects/chapter2/arrow-function-syntax.js
@@ -19,3 +19,17 @@ var object2 = {
 var bar = foo.call( object1 );
 
 bar.call( object2 ); // 2, not 3!
+
+// A common use case
+function foo() {
+    setTimeout(() => {
+        // 'this' here is lexically adopted from 'foo()'
+        console.log( this.a );
+    }, 100);
+}
+
+var object = {
+    a: 2
+}
+
+foo.call( object ); // 2
