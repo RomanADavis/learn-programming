84de1a8757586b292631b3278d114eee8f0fb779
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-04-02 03:57:49 +0000

    property coercion

---
+++ YDKJS/this-and-objects/chapter3/property-coercion.js
@@ -0,0 +1,11 @@
+var myObject = { };
+
+myObject[true] = "foo";
+myObject[3] = "bar";
+myObject[myObject] = "baz";
+
+myObject["true"];				// "foo"
+myObject["3"];					// "bar"
+myObject["[object Object]"];	// "baz"
+// properties can only be strings, and are automatically converted to strings
+// if you try to use something else
