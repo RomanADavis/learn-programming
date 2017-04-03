4555b5ef82066d7ed7d77850c501f3e99df06ea0
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-04-03 03:55:56 +0000

    constructed property names

---
+++ YDKJS/this-and-objects/chapter3/constructed-property-names.js
@@ -0,0 +1,11 @@
+// Because property names are just strings, they can be constructed, like so
+
+var prefix = "foo";
+
+var myObject = {
+	[prefix + "bar"]: "hello",
+	[prefix + "baz"]: "world"
+};
+
+myObject["foobar"]; // hello
+myObject["foobaz"]; // world
