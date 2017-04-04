7934a9058d993c9485592e6b78fc113abbfb1458
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-04-04 03:51:23 +0000

    methods

---
+++ YDKJS/this-and-objects/chapter3/methods.js
@@ -0,0 +1,17 @@
+// Methods in JS are just properties that return functions.
+
+function foo() {
+	console.log( "foo" );
+}
+
+var someFoo = foo;	// variable reference to `foo`
+
+var myObject = {
+	someFoo: foo
+};
+
+foo;				// function foo(){..}
+
+someFoo;			// function foo(){..}
+
+myObject.someFoo;	// function foo(){..}
