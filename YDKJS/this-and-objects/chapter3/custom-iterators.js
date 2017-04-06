58db443175c9981890b3790084bf356f23225067
Roman Alexander Davis <FluffyRibbit@gmail.com>
2017-04-06 10:44:43 -0400

    custom iterators

---
+++ YDKJS/this-and-objects/chapter3/custom-iterators.js
@@ -0,0 +1,20 @@
+// You can even make your own custom iterator, even an iterator that never ends!
+// The sky's the limit!
+
+var randoms = {
+	[Symbol.iterator]: function() {
+		return {
+			next: function() {
+				return { value: Math.random() };
+			}
+		};
+	}
+};
+
+var randoms_pool = [];
+for (var n of randoms) {
+	randoms_pool.push( n );
+
+	// don't proceed unbounded!
+	if (randoms_pool.length === 100) break;
+}
