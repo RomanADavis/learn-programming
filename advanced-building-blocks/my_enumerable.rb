87495a5657ee4eb459758284052f112510afed2a
Roman A. Davis <FluffyRibbit@gmail.com>
2017-04-12 08:18:38 -0400

    Added my_select and changed the file name from my_enumerables to my_enumerable

--- advanced-building-blocks/my_enumerable.rb
+++ advanced-building-blocks/my_enumerable.rb
@@ -6,4 +6,10 @@ module Enumerable
   def my_each_with_index
     self.length.times {|index| yield(self[index], index)}
   end
+
+  def my_select
+    result = []
+    self.each {|element| result << element if yield(element)}
+    result
+  end
 end
