34bf5f797fc52ff95e006eda2fa2356b60950f0e
Roman A. Davis <FluffyRibbit@gmail.com>
2017-04-12 08:08:34 -0400

    Added my_each_with_index. Seems to be working fine.

--- advanced-building-blocks/my_enumerables.rb
+++ advanced-building-blocks/my_enumerables.rb
@@ -2,4 +2,8 @@ module Enumerable
   def my_each
     self.length.times {|index| yield(self[index])}
   end
+
+  def my_each_with_index
+    self.length.times {|index| yield(self[index], index)}
+  end
 end
