4fa86e9331877fba6a251e2b34423a57bbeb33b9
Roman A. Davis <FluffyRibbit@gmail.com>
2017-04-12 09:09:15 -0400

    Went and wrote all the my_enumerables. Not tested yet, though.

--- advanced-building-blocks/my_enumerable.rb
+++ advanced-building-blocks/my_enumerable.rb
@@ -12,4 +12,37 @@ module Enumerable
     self.my_each {|element| result << element if yield(element)}
     result
   end
+
+  def my_all?
+    self.my_each {|element| return false unless yield(element)}
+    true
+  end
+
+  def my_any?
+    self.my_each {|element| return true if yield(element)}
+    false
+  end
+
+  def my_none?
+    !my_any?
+  end
+
+  def my_count
+    total = 0
+    self.my_each {|element| total += 1 if yield(element)}
+    total
+  end
+
+  def my_map(proc = nil)
+    result = []
+    self.my_each {|element| result << proc.call(element)} if proc
+    self.my_each {|element| result << yield(element)} if !proc
+    result
+  end
+
+  def my_inject
+    total = 0
+    self.my_each {|element| total = yield(element)}
+    total
+  end
 end
