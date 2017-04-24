8f589b0d6310c592c0cbdb4479beab3745300246
Roman Alexander Davis <fluffyribbit@gmail.com>
2017-04-24 03:54:31 -0400

    Removed references to self in my_enumerable.rb

--- odin-project/ruby/advanced-building-blocks/my_enumerable.rb
+++ odin-project/ruby/advanced-building-blocks/my_enumerable.rb
@@ -1,25 +1,25 @@
 module Enumerable
   def my_each
-    self.length.times {|index| yield(self[index])}
+    length.times {|index| yield(self[index])}
   end
 
   def my_each_with_index
-    self.length.times {|index| yield(self[index], index)}
+    length.times {|index| yield(self[index], index)}
   end
 
   def my_select
     result = []
-    self.my_each {|element| result << element if yield(element)}
+    my_each {|element| result << element if yield(element)}
     result
   end
 
   def my_all?
-    self.my_each {|element| return false unless yield(element)}
+    my_each {|element| return false unless yield(element)}
     true
   end
 
   def my_any?
-    self.my_each {|element| return true if yield(element)}
+    my_each {|element| return true if yield(element)}
     false
   end
 
@@ -29,20 +29,20 @@ module Enumerable
 
   def my_count
     total = 0
-    self.my_each {|element| total += 1 if yield(element)}
+    my_each {|element| total += 1 if yield(element)}
     total
   end
 
   def my_map(proc = nil)
     result = []
-    self.my_each {|element| result << proc.call(element)} if proc
-    self.my_each {|element| result << yield(element)} if !proc
+    my_each {|element| result << proc.call(element)} if proc
+    my_each {|element| result << yield(element)} if !proc
     result
   end
 
   def my_inject
     total = 0
-    self.my_each {|element| total = yield(element)}
+    my_each {|element| total = yield(element)}
     total
   end
 end
