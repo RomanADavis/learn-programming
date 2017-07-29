4d565cdf59d2d5ebc4ef521ce013ee35058a377f
Roman A Davis <FluffyRibbit@gmail.com>
2017-07-29 10:04:32 -0400

    Paddle moves

--- html5/games/breakout/javascript/breakout.js
+++ html5/games/breakout/javascript/breakout.js
@@ -8,4 +8,9 @@ window.onload = function() {
 
   var framesPerSecond = 30;
   setInterval(Game.tick, 1000 / framesPerSecond);
+
+  Game.canvas.addEventListener('mousemove', function(event) {
+    mousePosition = calculateMousePosition(event);
+    Game.paddle.x = mousePosition.x - Game.paddle.width / 2;
+  })
 }
--- html5/games/breakout/javascript/helpers.js
+++ html5/games/breakout/javascript/helpers.js
@@ -9,3 +9,11 @@ function colorCircle(centerX, centerY, radius, color) {
   Game.canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, color);
   Game.canvasContext.fill();
 }
+
+function calculateMousePosition(event) {
+  var rect = Game.canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = event.clientX - rect.left - root.scrollLeft;
+  var mouseY = event.clientY - rect.top - root.scrollTop;
+  return {x: mouseX, y: mouseY};
+}
