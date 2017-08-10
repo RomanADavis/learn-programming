73d5330057217ecfa17568bc4f740a7216305373
Roman A. Davis <FluffyRibbit@gmail.com>
2017-08-10 08:36:55 +0800

    Made a working sprite class

---
+++ html5/games/sprite/sprite.js
@@ -0,0 +1,38 @@
+var Sprite = {
+  new: function(image_source, x, y)	{
+    if(x == undefined) {
+      x = 0;
+    }
+
+    if (y == undefined) {
+      y = 0;
+    }
+
+    var sprite = {
+      x: x,
+      y: y,
+
+      draw: function() {
+        context.drawImage(this.image, this.x, this.y, this.image.width, this.image.height);
+      }
+    };
+
+    sprite.image = new Image();
+
+    //sprite.image.onload = sprite.draw();
+
+    sprite.image.src = image_source;
+
+    return sprite;
+  }
+};
+
+var canvas, context, ball;
+ball = Sprite.new('ball.png');
+
+window.onload = function() {
+  canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d");
+
+  ball.draw();
+};
