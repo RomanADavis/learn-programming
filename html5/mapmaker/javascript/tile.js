2986615ed86042929caffaa935bbb1a3ee64ab30
Roman A Davis <FluffyRibbit@gmail.com>
2017-08-26 18:33:50 -0400

    tile object

---
+++ html5/mapmaker/javascript/tile.js
@@ -0,0 +1,14 @@
+var Tile = {
+  new: function(tileset, left_x, top_y, sidelength) {
+    tile = {
+      tileset: tileset,
+      x: left_x,
+      y: top_y,
+      draw: function() {
+
+      }
+    };
+
+    return tile;
+  }
+};
