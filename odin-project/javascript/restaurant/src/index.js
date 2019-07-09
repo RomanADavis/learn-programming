2eac7d6456323d4defe5207b07d8f112d0793dcb
Roman Alexander Davis <fluffyribbit@gmail.com>
2019-07-10 06:41:29 +0800

    testing modularization from restaurant

--- odin-project/javascript/restaurant/src/index.js
+++ odin-project/javascript/restaurant/src/index.js
@@ -1,34 +1,3 @@
+import home from './home'
 
-function image(){
-  const element = document.createElement('div')
-  element.id = 'image'
-  const image = document.createElement('img')
-  image.src = 'https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg'
-
-  element.appendChild(image)
-
-  return element
-}
-
-function headline(){
-  const element = document.createElement('div')
-  element.id = 'headline'
-  const span = document.createElement('span')
-  span.innerHTML = "Here's the headline."
-  element.appendChild(span)
-  return element
-}
-
-function copy(){
-  const element = document.createElement('div')
-  const span = document.createElement('span')
-  span.innerHTML = "Here's some copy."
-  element.appendChild(span)
-  return element
-}
-
-const content = document.getElementById('content')
-
-content.appendChild(image())
-content.appendChild(headline())
-content.appendChild(copy())
+home()
